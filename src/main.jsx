230c778f3a3166a3df81818e563ecdbf911e768e
Vipul Singh <vipulsingh.1404@gmail.com>
2025-04-26 04:20:57 +0530

    redux-setup-done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
-
+import { store } from "./services/store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
