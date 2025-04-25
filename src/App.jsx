b961c4f71751c2f2d812c83065cddcd23e207f46
Vipul Singh <vipulsingh.1404@gmail.com>
2025-04-25 21:00:15 +0530

    tailwind-setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <div>HELLO</div>
+	  <div className="font-bold">HELLO</div>
+    </>
+  );
+};
+
+export default App;
